31b0b4e2eeb734b87668b6cc252167dfb30f47bc
Rishi Pathak <radhakpathak5@gmail.com>
2020-08-02 18:44:04 +0530

    Dark mode nearly complete

--- src/assets/js/src/script.js
+++ src/assets/js/src/script.js
@@ -1,4 +1,3 @@
-
 /*
 |--------------------------------------------------------------------------
 | Custom Javascript code
@@ -10,8 +9,51 @@
 |
 */
 
-$(function() {
+$(function () {
+  var t = localStorage.getItem("theme") || 0;
+  themeSwitcher();
+  $(".theme-switcher").click(function () {
+    if (t == 1) {
+      t = 0;
+      localStorage.setItem("theme", 0);
+    } else {
+      t = 1;
+      localStorage.setItem("theme", 1);
+    }
+    themeSwitcher();
+  });
+  console.clear();
+  console.log(t);
 
-
-
+  function themeSwitcher() {
+    if (t == 1) {
+      $("body").addClass("dark").removeClass("light");
+      $(".navbar-sun-light").attr("src", "assets/img/moon.svg");
+      $(".logo-dark").attr("src", "assets/img/logo-light.png");
+      $(".logo-light").attr("src", "assets/img/logo-dark.png");
+      $(".social-bg-light")
+        .addClass("social-bg-dark")
+        .removeClass("social-bg-light");
+      $(".slider-arrows-flash-dark")
+        .addClass("slider-arrows-flash-light")
+        .removeClass("slider-arrows-flash-dark");
+      t = 1;
+      localStorage.setItem("theme", 1);
+      console.log(t);
+    } else {
+      $("body").removeClass("dark").addClass("light");
+      $(".navbar-sun-light").attr("src", "assets/img/sun.svg");
+      $(".logo-dark").attr("src", "assets/img/logo-dark.png");
+      $(".logo-light").attr("src", "assets/img/logo-light.png");
+      $(".social-bg-dark")
+        .addClass("social-bg-light")
+        .removeClass("social-bg-dark");
+      $(".slider-arrows-flash-light")
+        .addClass("slider-arrows-flash-dark")
+        .removeClass("slider-arrows-flash-light");
+      t = 0;
+      localStorage.setItem("theme", 0);
+      console.log(t);
+    }
+  }
 });
