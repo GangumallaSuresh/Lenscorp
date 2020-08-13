c4beed7b6255f11209a3c7473f53e8044523718f
Rishi Pathak <radhakpathak5@gmail.com>
2020-08-13 22:58:54 +0530

    Carousel enabled

--- src/assets/plugin/thesaas/js/vendors/slick.js
+++ src/assets/plugin/thesaas/js/vendors/slick.js
@@ -1,30 +1,31 @@
-
 /**
  * Load required plugin.
  */
-require('slick-carousel');
-
+require("slick-carousel");
 
 /**
  * Configure the plugin.
  */
 
-+function($){
-  page.registerVendor('Slick');
++(function ($) {
+  page.registerVendor("Slick");
 
-  page.initSlick = function() {
-
-    $('[data-provide~="slider"]').each(function() {
+  page.initSlick = function () {
+    $('[data-provide~="slider"]').each(function () {
       var tag = $(this),
-          options = {
-            speed: 1000,
-            arrows: false,
-            centerPadding: '0',
-          };
+        options = {
+          speed: 1000,
+          arrows: false,
+          centerPadding: "0",
+          adaptiveHeight: true,
+        };
 
-      options = $.extend( options, page.getDataOptions(tag));
+      options = $.extend(options, page.getDataOptions(tag));
 
-      if (options.slidesToShow !== undefined || options.centerMode !== undefined) {
+      if (
+        options.slidesToShow !== undefined ||
+        options.centerMode !== undefined
+      ) {
         var scrollOn768 = 1;
         if (options.slidesToScroll !== undefined) {
           if (options.slidesToScroll > 1) {
@@ -32,29 +33,26 @@ require('slick-carousel');
           }
         }
 
-        options.responsive = [{
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: scrollOn768,
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerPadding: '0px',
-          }
-        }];
+        options.responsive = [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: scrollOn768,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerPadding: "0px",
+            },
+          },
+        ];
       }
 
       tag.slick(options);
     });
-
-  }
-
-}(jQuery);
-
-
-
+  };
+})(jQuery);
